bbf1e6f9b1ac3c4fd6506882ad8f9a6a5f669c1d
Diego <danoguera@gmail.com>
2020-05-07 23:57:32 -0500

    Fixed repeater

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      word: ''
+    }
+  } 
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" onChange={this.handleText} value={this.state.word} />
+        <p className="repeater">{this.state.word}</p>
       </div>
     );
+  }
+
+  handleText = (e) => {
+    this.setState({ word: e.target.value });
   }
 }
 
